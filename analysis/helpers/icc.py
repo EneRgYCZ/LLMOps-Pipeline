fix: correct ICC(2,1) formula to include between-run mean square

analysis/helpers/icc.py
@@ -14,6 +14,7 @@ class ReliabilityAnalysis:
 
         grand_mean = data.mean()
         ms_rows = k * np.sum((data.mean(axis=1) - grand_mean) ** 2) / (n - 1)
+        ms_cols = n * np.sum((data.mean(axis=0) - grand_mean) ** 2) / (k - 1)
         ss_err = np.sum(
             (
                 data
@@ -25,4 +26,6 @@ class ReliabilityAnalysis:
         )
         ms_err = ss_err / ((n - 1) * (k - 1))
 
-        return (ms_rows - ms_err) / (ms_rows + (k - 1) * ms_err)
+        return (ms_rows - ms_err) / (
+            ms_rows + (k - 1) * ms_err + (k / n) * (ms_cols - ms_err)
+        )